14bcb963ea8dcc4dff6d1ceb7f7431a9d391cba6
Daniel Chen <dychen@caltech.edu>
2013-08-18 11:17:08 -0700

    Added styling with bootstrap/sass and refactored. Added a contact page. Made routes pretty.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,27 +1,38 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+    let(:title_base) { "Ruby on Rails Tutorial Sample App" }
+
     describe "Home page" do
         it "should have correct title and the content 'Sample App'" do
-            visit '/static_pages/home'
-            expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+            visit root_path
+            expect(page).to have_title("#{title_base}")
+            expect(page).not_to have_title("| Home")
             expect(page).to have_content("Sample App")
         end
     end
 
     describe "Help page" do
         it "should have the correct title and the content 'Help'" do
-            visit '/static_pages/help'
-            expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+            visit help_path
+            expect(page).to have_title("#{title_base} | Help")
             expect(page).to have_content("Help")
         end
     end
 
     describe "About page" do
         it "should have the correct title and the content 'About Us'" do
-            visit '/static_pages/about'
-            expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+            visit about_path
+            expect(page).to have_title("#{title_base} | About Us")
             expect(page).to have_content("About Us")
+        end
+    end
+
+    describe "Contact page" do
+        it "should have the correct title and the content 'Contact'" do
+            visit contact_path
+            expect(page).to have_title("#{title_base} | Contact")
+            expect(page).to have_content("Contact")
         end
     end
 end
